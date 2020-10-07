90031704c9bd38710b83528cc61890376967b767
CharlesvanLieshout <66746563+CharlesvanLieshout@users.noreply.github.com>
2020-10-07 11:38:52 +0200

    Add files via upload

---
+++ eerste Javascript/script.js
@@ -0,0 +1,9 @@
+console.log("Hello Winc Academy");
+let name = "Charles";
+console.log(name);
+console.log(4 + 4);
+console.log("4" + "4");
+console.log(14 - 2);
+console.log(4 * 234);
+console.log(24 / 4);
+console.log(38 % 3);
